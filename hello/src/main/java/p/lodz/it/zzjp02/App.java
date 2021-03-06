8f0a9dd3394e3319821bf4ca7da69e683fed5332
dlondka <londka99@gmail.com>
2021-03-06 14:35:40 +0100

    Add app that creates instance of person and prints hello im + name

---
+++ hello/src/main/java/p/lodz/it/zzjp02/App.java
@@ -0,0 +1,12 @@
+package p.lodz.it.zzjp02;
+
+import p.lodz.it.zzjp.Person;
+
+public class App {
+
+    public static void main( String[] args ) {
+        Person person = new Person();
+        person.setName("Daniel");
+        person.sayHello();
+    }
+}
